147fcfe0e337322d933c7153f0e6377b053afb31
Sheng Yunzhou <shengyunzhou@gmail.com>
2020-05-10 15:07:34 +0800

    Add answer to day one part one

--- src/advent_of_code/day_one.clj
+++ src/advent_of_code/day_one.clj
@@ -1,3 +1,17 @@
-(ns advent-of-code.day-one)
+(ns advent-of-code.day-one
+  (:require [clojure.java.io :as io]))
 
-(print "hello")
+(defn read-file [path]
+  (with-open [rdr (io/reader path)]
+    (doall (line-seq rdr))))
+
+(defn get-fuel
+  "gets fuel by mass"
+  [mass]
+  (- (int (Math/floor (/ mass 3))) 2))
+
+(->> "resources/dayone_part_one.txt"
+     read-file
+     (map read-string)
+     (map get-fuel)
+     (reduce +))

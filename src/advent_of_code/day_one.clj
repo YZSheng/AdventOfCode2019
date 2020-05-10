798ce89464f3471787ffec82aa7b6364d905b8a0
Sheng Yunzhou <shengyunzhou@gmail.com>
2020-05-10 15:47:50 +0800

    Add solution to part two

--- src/advent_of_code/day_one.clj
+++ src/advent_of_code/day_one.clj
@@ -8,7 +8,7 @@
 (defn get-fuel
   "gets fuel by mass"
   [mass]
-  (- (int (Math/floor (/ mass 3))) 2))
+  (- (quot mass 3) 2))
 
 (defn find-sum-of-fuels [mass-list]
   (->> mass-list
@@ -19,3 +19,21 @@
 (->> "resources/day_one_part_one.txt"
      read-file
      find-sum-of-fuels)
+
+
+;; part two
+
+(defn fuel-required [mass]
+  (reduce + (rest (take-while pos? (iterate get-fuel mass)))))
+
+(defn find-sum-of-fuels-of-fuel [mass-list]
+  (->> mass-list
+       (map read-string)
+       (map fuel-required)
+       (reduce +)))
+
+;; 5106629
+
+(->> "resources/day_one_part_one.txt"
+     read-file
+     find-sum-of-fuels-of-fuel)
